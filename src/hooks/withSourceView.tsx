88d632e0476fcbd7b2e2759280485336fcde9fd3
yamdeng <yamdeng@gmail.com>
2024-06-30 14:59:09 +0900

    리액트 개발 가이드 기본 와꾸 fix

--- src/hooks/withSourceView.tsx
+++ src/hooks/withSourceView.tsx
@@ -1,28 +1,53 @@
 import { useState } from 'react';
 import Config from '@/config/Config';
 import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
-import { dark } from 'react-syntax-highlighter/dist/esm/styles/prism';
+import { darcula, oneLight } from 'react-syntax-highlighter/dist/esm/styles/prism';
 
 function withSourceView(WrappedComponent) {
   return function ParentComponent(props) {
+    const sourceTheme = localStorage.getItem('sourceTheme') || 'dark';
+    console.log(`sourceTheme : ${sourceTheme}`);
     const { menuInfo } = props;
     const [viewSource, setViewSource] = useState(false);
+    const [viewSourceTheme, setViewSourceTheme] = useState(sourceTheme);
 
     const changeViewSource = (event) => {
       const checked = event.target.checked;
       setViewSource(checked);
     };
+
+    const changeSourceTheme = (event) => {
+      const value = event.target.value;
+      setViewSourceTheme(value);
+      localStorage.setItem('sourceTheme', value);
+    };
+
     const hrefString = Config.hrefBasePath + menuInfo.moduleDirectory + '/' + menuInfo.path + Config.reactFileExtension;
 
     return (
       <div style={{ marginRight: viewSource ? '50%' : 0 }}>
         <div className="guide-detail-top-common">
-          <div className="guide-detail-top-title">{menuInfo.title}</div>
-          <a href={hrefString}>{menuInfo.path}</a>{' '}
-          <input type="checkbox" checked={viewSource} onChange={changeViewSource} /> 소스보기
+          <a href={hrefString}>{menuInfo.title}</a>{' '}
+          <input type="checkbox" checked={viewSource} onChange={changeViewSource} /> 소스보기{' '}
+          <select
+            name="source-view-theme"
+            value={viewSourceTheme}
+            onChange={(e) => changeSourceTheme(e)}
+            style={{
+              padding: 3,
+            }}
+          >
+            <option value="dark">dark</option>
+            <option value="light">light</option>
+          </select>{' '}
         </div>
         <div className="source-raw-view" style={{ width: viewSource ? '50%' : 0 }}>
-          <SyntaxHighlighter language="javascript" style={dark}>
+          <SyntaxHighlighter
+            language="javascript"
+            style={viewSourceTheme === 'dark' ? darcula : oneLight}
+            showLineNumbers
+            wrapLongLines
+          >
             {menuInfo.fileRawString}
           </SyntaxHighlighter>
         </div>
