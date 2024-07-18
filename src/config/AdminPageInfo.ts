e4e2e64e9fc42dad8097ce8724c1108c980e91b1
jessica0943 <jessica0943@gmail.com>
2024-07-18 10:47:34 +0900

    관리자 페이지 설정 정보 추가

--- src/config/AdminPageInfo.ts
+++ src/config/AdminPageInfo.ts
@@ -1,6 +1,7 @@
 import AdminBasicList from '@/components/publish/admin/AdminBasicList';
 import AdminBasicForm from '@/components/publish/admin/AdminBasicForm';
 import AdminBasicForm2 from '@/components/publish/admin/AdminBasicForm2';
+import AdminBasicFormOther from '@/components/publish/admin/AdminBasicFormOther';
 
 const AdminPageInfo: any = {};
 
@@ -23,6 +24,13 @@ AdminPageInfo.list = [
     title: '기본 폼',
     Component: AdminBasicForm2,
     path: 'AdminBasicForm2',
+    description: '',
+    success: false,
+  },
+  {
+    title: 'AdminBasicFormOther',
+    Component: AdminBasicFormOther,
+    path: 'AdminBasicFormOther',
     description: '',
     success: false,
   },
