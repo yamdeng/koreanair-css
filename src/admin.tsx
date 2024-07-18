72ebf280c912a6caef2740757567d6e90f7ad77b
yamdeng <yamdeng@gmail.com>
2024-07-18 20:28:10 +0900

    admin, user 분류

--- src/admin.tsx
+++ src/admin.tsx
@@ -9,7 +9,7 @@ import 'react-toastify/dist/ReactToastify.css';
 import 'ag-grid-community/styles/ag-grid.css'; // Mandatory CSS required by the grid
 import 'ag-grid-community/styles/ag-theme-quartz.css'; // Optional Theme applied to the grid
 import '@toast-ui/editor/dist/toastui-editor.css';
-import './resources/css/import.scss';
+import './resources/css/importadmin.scss';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
