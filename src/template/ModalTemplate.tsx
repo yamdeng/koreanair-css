00bdc568fa8dc2a21bd9132e44cb29a29ebdbf43
yamdeng <yamdeng@gmail.com>
2024-07-22 20:32:59 +0900

    css 가이드 문서 정리

--- src/template/ModalTemplate.tsx
+++ src/template/ModalTemplate.tsx
@@ -1,7 +1,62 @@
+import { useState } from 'react';
+import Modal from 'react-modal';
+
+function TestModal(props) {
+  const { displayModal, closeModal } = props;
+
+  /*
+
+    overlayClassName : alert-modal-overlay, middle-modal-overlay, full-modal-overlay
+     : 크기에 따라 클래스 정의
+
+    className : {커스텀}-modal-content
+     모달 마다 별도의 class를 정의해서 커스텀하게 관리
+
+  */
+  return (
+    <Modal
+      shouldCloseOnOverlayClick={false}
+      isOpen={displayModal}
+      ariaHideApp={false}
+      overlayClassName={'alert-modal-overlay'}
+      className={'alert-modal-content'}
+      onRequestClose={() => {
+        closeModal();
+      }}
+    >
+      <div className="popup-container">
+        <h3 className="pop_title">제목</h3>
+        <p className="pop_cont" dangerouslySetInnerHTML={{ __html: '' }} />
+        <div className="pop_btns">
+          <button className="btn_text text_color_neutral-10 btn_confirm" onClick={closeModal}>
+            확인
+          </button>
+        </div>
+        <span className="pop_close" onClick={closeModal}>
+          {/* <i className="fas fa-times"></i> */}X
+        </span>
+      </div>
+    </Modal>
+  );
+}
+
 function ModalTemplate() {
+  const [displayModal, setDisplayModal] = useState(false);
+  const closeModal = () => {
+    setDisplayModal(false);
+  };
+
   return (
     <>
-      <p>ModalTemplate</p>
+      <div>
+        신규파일명
+        <p>
+          <button className="button" onClick={() => setDisplayModal(true)}>
+            alert modal open
+          </button>
+        </p>
+        <TestModal displayModal={displayModal} closeModal={closeModal} />
+      </div>
     </>
   );
 }
