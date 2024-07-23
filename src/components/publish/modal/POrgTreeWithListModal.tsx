4724e26706347f7869216f22c5bb52f39eab0339
kimjuhee4 <kimjuhee4@hanmail.net>
2024-07-23 13:38:03 +0900

    트리 작업중

--- src/components/publish/modal/POrgTreeWithListModal.tsx
+++ src/components/publish/modal/POrgTreeWithListModal.tsx
@@ -4,7 +4,7 @@ import Modal from 'react-modal';
 import { getAllData } from '@/data/grid/example-data-new';
 import { testColumnInfos } from '@/data/grid/table-column';
 import AppTable from '@/components/common/AppTable';
-import PBoxForm from '@/components/common/PBoxForm';
+import { DatePicker, Select as AntSelect } from 'antd';
 
 /* treeData 가공 */
 const x = 3;
@@ -37,6 +37,7 @@ generateData(z);
 /* treeData 가공 end*/
 
 function TreeModal(props) {
+  const [inputValue, setInputValue] = useState('');
   const { displayModal, closeModal } = props;
   const [expandedKeys] = useState(['0-0', '0-0-0', '0-0-0-0']);
   const rowData = getAllData();
@@ -67,7 +68,91 @@ function TreeModal(props) {
               />
             </div>
             <div className="pop_cont_form">
-              <PBoxForm />
+              <div className="boxForm">
+                <div className="form-table">
+                  <div className="form-cell wid50">
+                    <span className="form-group wid100 mr5">
+                      <input
+                        type="text"
+                        className="form-tag"
+                        name="title"
+                        value={inputValue}
+                        onChange={(event) => {
+                          setInputValue(event.target.value);
+                        }}
+                      />
+                      <label className="f-label">
+                        Sbject <span className="required">*</span>
+                      </label>
+                    </span>
+                  </div>
+                  <div className="form-cell wid50">
+                    <span className="form-group wid100">
+                      <AntSelect
+                        style={{ width: '100%' }}
+                        options={[
+                          {
+                            value: 'jack',
+                            label: 'Jack',
+                          },
+                          {
+                            value: 'lucy',
+                            label: 'Lucy',
+                          },
+                          {
+                            value: 'Yiminghe',
+                            label: 'yiminghe',
+                          },
+                          {
+                            value: 'disabled',
+                            label: 'Disabled',
+                            disabled: true,
+                          },
+                        ]}
+                      />
+                    </span>
+                  </div>
+                </div>
+
+                <div className="form-table">
+                  <div className="form-cell ">
+                    <span className="form-group wid100">
+                      <span className="form-group wid100">
+                        <AntSelect
+                          style={{ width: '100%' }}
+                          options={[
+                            {
+                              value: 'jack',
+                              label: 'Jack',
+                            },
+                            {
+                              value: 'lucy',
+                              label: 'Lucy',
+                            },
+                            {
+                              value: 'Yiminghe',
+                              label: 'yiminghe',
+                            },
+                            {
+                              value: 'disabled',
+                              label: 'Disabled',
+                              disabled: true,
+                            },
+                          ]}
+                        />
+                      </span>
+                    </span>
+                  </div>
+                  <div className="form-cell wid50">
+                    <span className="form-group form-glow">
+                      <DatePicker status="" /> {/* status="error" */}
+                      <span>~</span>
+                      <DatePicker status="" />
+                      {/* <TimePicker minuteStep={15} secondStep={10} hourStep={1} status="error" /> */}
+                    </span>
+                  </div>
+                </div>
+              </div>
               <AppTable rowData={rowData} columns={columns} />
             </div>
           </div>
