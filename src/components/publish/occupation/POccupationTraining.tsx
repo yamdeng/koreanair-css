9e0d71d40c76553af3984e39ae91b75cc6112daa
안용성 <pj.yamdeng2@kalmate.net>
2024-08-05 09:07:59 +0900

    공통컴포넌트 convert

--- src/components/publish/occupation/POccupationTraining.tsx
+++ src/components/publish/occupation/POccupationTraining.tsx
@@ -3,6 +3,12 @@ import { DatePicker, TimePicker, Select as AntSelect } from 'antd';
 import AppTable from '@/components/common/AppTable';
 import { getAllData } from '@/data/grid/example-data-new';
 import { testColumnInfos } from '@/data/grid/table-column';
+import AppSearchnput from '@/components/common/AppSearchnput';
+import AppTextInput from '@/components/common/AppTextInput';
+import AppSelect from '@/components/common/AppSelect';
+import AppDatePicker from '@/components/common/AppDatePicker';
+import AppRangeDatePicker from '@/components/common/AppRangeDatePicker';
+import AppTimePicker from '@/components/common/AppTimePicker';
 
 function POccupationTraining() {
   const [inputValue, setInputValue] = useState('');
@@ -19,88 +25,17 @@ function POccupationTraining() {
           <div className="form-table">
             <div className="form-cell wid50">
               <div className="form-group wid100">
-                <AntSelect
-                  id="select1"
-                  style={{ width: '100%' }}
-                  className="label-select"
-                  status=""
-                  options={[
-                    {
-                      value: 'jack',
-                      label: 'Jack',
-                    },
-                    {
-                      value: 'lucy',
-                      label: 'Lucy',
-                    },
-                    {
-                      value: 'Yiminghe',
-                      label: 'yiminghe',
-                    },
-                    {
-                      value: 'disabled',
-                      label: 'Disabled',
-                      disabled: true,
-                    },
-                  ]}
-                />
-                <label className="f-label" htmlFor="select1">
-                  부문 {/*<span className="required">*</span>*/}
-                </label>
-                {/*<span className="errorText">auto complete error message</span>*/}
+                <AppSelect label={'부문'} />
               </div>
             </div>
             <div className="form-cell wid50">
               <div className="form-group wid100 mr5">
-                <input
-                  id="firstInput1"
-                  type="text"
-                  className="form-tag"
-                  name="title"
-                  value={inputValue}
-                  onChange={(event) => {
-                    setInputValue(event.target.value);
-                  }}
-                />
-                <label className="f-label" htmlFor="firstInput1">
-                  부서 {/*<span className="required">*</span>*/}
-                </label>
-                <button className="sch-btnClear"></button>
-                <button type="button" className="icon-sch"></button>
+                <AppSearchnput label="부서" />
               </div>
             </div>
             <div className="form-cell wid50">
               <div className="form-group wid100">
-                <AntSelect
-                  id="select1"
-                  style={{ width: '100%' }}
-                  className="label-select"
-                  status=""
-                  options={[
-                    {
-                      value: 'jack',
-                      label: 'Jack',
-                    },
-                    {
-                      value: 'lucy',
-                      label: 'Lucy',
-                    },
-                    {
-                      value: 'Yiminghe',
-                      label: 'yiminghe',
-                    },
-                    {
-                      value: 'disabled',
-                      label: 'Disabled',
-                      disabled: true,
-                    },
-                  ]}
-                />
-                <label className="f-label" htmlFor="select1">
-                  재난유형
-                  {/*<span className="required">*</span>*/}
-                </label>
-                {/*<span className="errorText">auto complete error message</span>*/}
+                <AppSelect label={'재난유형'} />
               </div>
             </div>
           </div>
@@ -109,39 +44,18 @@ function POccupationTraining() {
               <div className="form-group form-glow">
                 <div className="df">
                   <div className="date1">
-                    <DatePicker status="" id="date1" className="label-picker" placeholder="" /> {/* status="error" */}
-                    <label className="f-label" htmlFor="date1">
-                      훈련실시기간 <span className="required"></span>
-                    </label>
-                    {/*<span className="errorText">date1 error</span>*/}
+                    <AppDatePicker label="훈련실시시간" />
                   </div>
                   <span className="unt">~</span>
                   <div className="date2">
-                    <DatePicker status="" id="date2" className="label-picker" placeholder="" /> {/* status="error" */}
-                    <label className="f-label" htmlFor="date2">
-                      date1-2 <span className="required"></span>
-                    </label>
-                    {/*<span className="errorText">date2 error</span>*/}
+                    <AppDatePicker label="훈련실시시간2" />
                   </div>
                 </div>
               </div>
             </div>
             <div className="form-cell wid100">
               <div className="form-group wid100">
-                <input
-                  id="firstInput3"
-                  type="text"
-                  className="form-tag"
-                  name="title"
-                  value={inputValue}
-                  onChange={(event) => {
-                    setInputValue(event.target.value);
-                  }}
-                />
-                <label className="f-label" htmlFor="firstInput3">
-                  훈련명 {/*<span className="errorText">date2 error</span>*/}
-                </label>
-                <button className="btnClear"></button>
+                <AppSearchnput label="훈련명" />
               </div>
             </div>
           </div>
