c59137c02e46c08d4013833082f12adc64f777d7
kimjuhee4 <kimjuhee4@hanmail.net>
2024-08-07 10:21:03 +0900

    공지사항상세

--- src/components/publish/occupation/POccupationPortal2detail.tsx
+++ src/components/publish/occupation/POccupationPortal2detail.tsx
@@ -4,16 +4,137 @@ function POccupationPortal2detail() {
       <div className="conts-title">
         <h2>공지사항</h2>
       </div>
-      {/* 입력영역 */}
-      <div className="editbox"></div>
-      {/*//입력영역*/}
+      {/*상세페이지*/}
+      <div className="editbox">
+        <div className="form-table">
+          <div className="form-cell wid100">
+            <div className="form-group wid100">
+              <div className="box-view-list">
+                <ul className="view-list">
+                  <li className="accumlate-list">
+                    <label className="t-label">본부</label>
+                    <span className="text-desc">정비</span>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div className="form-cell wid100">
+            <div className="form-group wid100">
+              <div className="box-view-list">
+                <ul className="view-list">
+                  <li className="accumlate-list">
+                    <label className="t-label">등록자</label>
+                    <span className="text-desc">홍길동</span>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div className="form-cell wid100">
+            <div className="form-group wid100">
+              <div className="box-view-list">
+                <ul className="view-list">
+                  <li className="accumlate-list">
+                    <label className="t-label">등록일자</label>
+                    <span className="text-desc">2024-08-01</span>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div className="form-cell wid100">
+            <div className="form-group wid100">
+              <div className="box-view-list">
+                <ul className="view-list">
+                  <li className="accumlate-list">
+                    <label className="t-label">KSMS 사이렌 여부</label>
+                    <span className="text-desc">아니오</span>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div className="form-cell wid100">
+            <div className="form-group wid100">
+              <div className="box-view-list">
+                <ul className="view-list">
+                  <li className="accumlate-list">
+                    <label className="t-label">상위 표출 여부</label>
+                    <span className="text-desc">아니오</span>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+        <hr className="line"></hr>
+        <div className="form-table">
+          <div className="form-cell wid100">
+            <div className="form-group wid100">
+              <div className="box-view-list">
+                <ul className="view-list">
+                  <li className="accumlate-list">
+                    <label className="t-label">제목</label>
+                    <span className="text-desc">공지사항 제목입니다.</span>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+        <hr className="line"></hr>
+        <div className="form-table">
+          <div className="form-cell wid100">
+            <div className="form-group wid100">
+              <div className="box-view-list">
+                <ul className="view-list">
+                  <li className="accumlate-list">
+                    <label className="t-label">내용</label>
+                    <span className="text-desc">
+                      공지사항 내용입니다.
+                      <br />
+                      공지사항 내용입니다.공지사항 내용입니다.
+                      <br />
+                      공지사항 내용입니다.공지사항 내용입니다.공지사항 내용입니다.공지사항 내용입니다.
+                    </span>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+        <hr className="line"></hr>
+        <div className="form-table">
+          <div className="form-cell wid100">
+            <div className="form-group wid100">
+              <div className="box-view-list">
+                <ul className="view-list">
+                  <li className="accumlate-list">
+                    <label className="t-label">첨부파일(작업중)</label>
+                    <span className="text-desc">첨부파일입니다.</span>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+        <hr className="line"></hr>
+      </div>
+      {/*//상세페이지*/}
       {/* 하단버튼영역 */}
       <div className="contents-btns">
         <button type="button" name="button" className="btn_text text_color_neutral-10 btn_confirm">
           <span className="share">공유</span>
         </button>
         <button type="button" name="button" className="btn_text text_color_neutral-10 btn_confirm">
-          신규
+          수정
+        </button>
+        <button type="button" name="button" className="btn_text text_color_neutral-10 btn_confirm">
+          삭제
+        </button>
+        <button type="button" name="button" className="btn_text text_color_neutral-10 btn_confirm">
+          목록
         </button>
       </div>
       {/*//하단버튼영역*/}
--- src/components/publish/occupation/POccupationPortal2edit.tsx
+++ src/components/publish/occupation/POccupationPortal2edit.tsx
@@ -68,7 +68,7 @@ function POccupationPortal2edit() {
                 <input className="upload-name" value="첨부파일" placeholder="첨부파일" />
                 <input type="file" id="file" />
                 <label htmlFor="file" className="f-label">
-                  파일찾기
+                  파일찾기(작업중)
                 </label>
 
                 <button>추가</button>
