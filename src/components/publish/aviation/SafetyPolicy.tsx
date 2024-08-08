d618413014dee3389a8b710e493ead7626a758c9
jessica0943 <jessica0943@gmail.com>
2024-08-08 09:13:05 +0900

    공통CSS 수정

--- src/components/publish/aviation/SafetyPolicy.tsx
+++ src/components/publish/aviation/SafetyPolicy.tsx
@@ -17,12 +17,12 @@ function SafetyPolicy() {
         <h2>안전정책</h2>
       </div>
       {/*상세페이지*/}
-      <div className="editbox">
+      <div className="editbox NoLine">
         <div className="form-table">
           <div className="form-cell wid100">
             <div className="form-group wid100">
               <div className="img">
-                <img src={SafetyImage} alt="Safety Policy" />
+                <img src={SafetyImage} className="img-thumbnail" alt="Safety Policy" />
               </div>
             </div>
           </div>
