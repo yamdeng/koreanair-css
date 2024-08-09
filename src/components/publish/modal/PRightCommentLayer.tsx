c9c5bfbcd8511135a001946763ca9c737b9e9c20
안용성 <pj.yamdeng2@kalmate.net>
2024-08-09 13:07:40 +0900

    우측 레이어 영역 반영

--- src/components/publish/modal/PRightCommentLayer.tsx
+++ src/components/publish/modal/PRightCommentLayer.tsx
@@ -1,7 +1,25 @@
+import { useState } from 'react';
+
 function PRightCommentLayer() {
+  const [isOpen, setIsOpen] = useState(false);
+  const handleClick = () => {
+    setIsOpen(true);
+  };
+  let applyClassName = 'laye-test';
+  if (isOpen) {
+    applyClassName = applyClassName + ' active';
+  }
+
   return (
     <>
-      <div>PRightCommentLayer</div>
+      <div>PRightCommentLayer page</div>
+      적용된 클래스 : {applyClassName}
+      <div className="contents-btns" style={{ textAlign: 'left' }}>
+        <button name="button" className="btn_text text_color_neutral-10 btn_confirm" onClick={handleClick}>
+          토글 우측 레이어
+        </button>
+      </div>
+      <div className={applyClassName}>우측 레이어영역입니다.</div>
     </>
   );
 }
