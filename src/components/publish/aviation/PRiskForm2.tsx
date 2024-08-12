a661872ab12ff9c2c560ca3653e174dc77f943b9
kimjuhee4 <kimjuhee4@hanmail.net>
2024-08-12 12:31:59 +0900

    라디오/체크박스 수정

--- src/components/publish/aviation/PRiskForm2.tsx
+++ src/components/publish/aviation/PRiskForm2.tsx
@@ -79,19 +79,21 @@ function PRiskForm2() {
                         <AppTextInput label="기상조건" />
                       </div>
                     </div>
-                    <div className="group-box-wrap wid50">
-                      <span className="txt">
-                        SPI 여부<span className="required">*</span>
-                      </span>
-                      <div className="radio-wrap">
-                        <label>
-                          <input type="radio" checked />
-                          <span>YES</span>
-                        </label>
-                        <label>
-                          <input type="radio" />
-                          <span>NO</span>
-                        </label>
+                    <div className="form-cell wid50">
+                      <div className="group-box-wrap wid100">
+                        <span className="txt">
+                          SPI 여부<span className="required">*</span>
+                        </span>
+                        <div className="radio-wrap error">
+                          <label>
+                            <input type="radio" checked />
+                            <span>YES</span>
+                          </label>
+                          <label>
+                            <input type="radio" />
+                            <span>NO</span>
+                          </label>
+                        </div>
                       </div>
                     </div>
                   </div>
