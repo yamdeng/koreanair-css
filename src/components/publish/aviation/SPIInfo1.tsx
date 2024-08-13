89b77b3f182f2c5740d95f6a83456f0b5e54c902
jessica0943 <jessica0943@gmail.com>
2024-08-13 14:40:21 +0900

    테이블 추가

--- src/components/publish/aviation/SPIInfo1.tsx
+++ src/components/publish/aviation/SPIInfo1.tsx
@@ -48,19 +48,359 @@ function SPIInfo1() {
       </div>
       {/* //검색영역 */}
 
-      {/*그리드영역 */}
-      <div className="">
-        <AppTable rowData={rowData} columns={columns} />
+      {/*테이블영역*/}
+      <div className="info-box">
+        <p className="h4">PAX/CGO 별 비행편 수</p>
+        <table className="info-board">
+          <colgroup>
+            <col width="8%" />
+            <col width="7%" />
+            <col width="7%" />
+            <col width="7%" />
+            <col width="7%" />
+            <col width="7%" />
+            <col width="7%" />
+            <col width="7%" />
+            <col width="7%" />
+            <col width="7%" />
+            <col width="7%" />
+            <col width="7%" />
+            <col width="7%" />
+            <col width="7%" />
+          </colgroup>
+          <tr>
+            <th>PAX/CGO</th>
+            <th>01</th>
+            <th>02</th>
+            <th>03</th>
+            <th>04</th>
+            <th>05</th>
+            <th>06</th>
+            <th>07</th>
+            <th>08</th>
+            <th>09</th>
+            <th>10</th>
+            <th>11</th>
+            <th>12</th>
+            <th>TTL</th>
+          </tr>
+          <tr>
+            <td>PAX</td>
+            <td className="right">872</td>
+            <td className="right">561</td>
+            <td className="right">3,312</td>
+            <td className="right">7,565</td>
+            <td className="right">124</td>
+            <td className="right">5,968</td>
+            <td className="right">0</td>
+            <td className="right">0</td>
+            <td className="right">0</td>
+            <td className="right">0</td>
+            <td className="right">0</td>
+            <td className="right">0</td>
+            <td className="right">17,089</td>
+          </tr>
+          <tr>
+            <td>CGO</td>
+            <td className="right">98</td>
+            <td className="right">87</td>
+            <td className="right">970</td>
+            <td className="right">1,276</td>
+            <td className="right">96</td>
+            <td className="right">1,142</td>
+            <td className="right">0</td>
+            <td className="right">0</td>
+            <td className="right">0</td>
+            <td className="right">0</td>
+            <td className="right">0</td>
+            <td className="right">0</td>
+            <td className="right">2,871</td>
+          </tr>
+          <tr className="cons-bottom">
+            <th>Total</th>
+            <td className="right">970</td>
+            <td className="right">638</td>
+            <td className="right">4,282</td>
+            <td className="right">8,841</td>
+            <td className="right">970</td>
+            <td className="right">7,110</td>
+            <td className="right">0</td>
+            <td className="right">0</td>
+            <td className="right">0</td>
+            <td className="right">0</td>
+            <td className="right">0</td>
+            <td className="right">0</td>
+            <td className="right">19,960</td>
+          </tr>
+        </table>
       </div>
-      {/*//그리드영역 */}
-
-      {/* 하단버튼영역 */}
-      <div className="contents-btns">
-        <button type="button" name="button" className="btn_text text_color_neutral-10 btn_confirm">
-          신규
-        </button>
+      <div className="info-box">
+        <p className="h4">국내/국제선 비행편 수</p>
+        <table className="info-board">
+          <colgroup>
+            <col width="8%" />
+            <col width="7%" />
+            <col width="7%" />
+            <col width="7%" />
+            <col width="7%" />
+            <col width="7%" />
+            <col width="7%" />
+            <col width="7%" />
+            <col width="7%" />
+            <col width="7%" />
+            <col width="7%" />
+            <col width="7%" />
+            <col width="7%" />
+            <col width="7%" />
+          </colgroup>
+          <tr>
+            <th>국내/국제</th>
+            <th>01</th>
+            <th>02</th>
+            <th>03</th>
+            <th>04</th>
+            <th>05</th>
+            <th>06</th>
+            <th>07</th>
+            <th>08</th>
+            <th>09</th>
+            <th>10</th>
+            <th>11</th>
+            <th>12</th>
+            <th>TTL</th>
+          </tr>
+          <tr>
+            <td>PAX</td>
+            <td className="right">280</td>
+            <td className="right">561</td>
+            <td className="right">855</td>
+            <td className="right">2,648</td>
+            <td className="right">872</td>
+            <td className="right">2,101</td>
+            <td className="right">0</td>
+            <td className="right">0</td>
+            <td className="right">0</td>
+            <td className="right">0</td>
+            <td className="right">0</td>
+            <td className="right">0</td>
+            <td className="right">5,884</td>
+          </tr>
+          <tr>
+            <td>CGO</td>
+            <td className="right">690</td>
+            <td className="right">87</td>
+            <td className="right">2,184</td>
+            <td className="right">6,193</td>
+            <td className="right">98</td>
+            <td className="right">5,009</td>
+            <td className="right">0</td>
+            <td className="right">0</td>
+            <td className="right">0</td>
+            <td className="right">0</td>
+            <td className="right">0</td>
+            <td className="right">0</td>
+            <td className="right">14,076</td>
+          </tr>
+          <tr className="cons-bottom">
+            <th>Total</th>
+            <td className="right">970</td>
+            <td className="right">638</td>
+            <td className="right">3,039</td>
+            <td className="right">8,841</td>
+            <td className="right">970</td>
+            <td className="right">7,110</td>
+            <td className="right">0</td>
+            <td className="right">0</td>
+            <td className="right">0</td>
+            <td className="right">0</td>
+            <td className="right">0</td>
+            <td className="right">0</td>
+            <td className="right">19,960</td>
+          </tr>
+        </table>
       </div>
-      {/*//하단버튼영역*/}
+      <div className="info-box">
+        <p className="h4">Fleet 별 비행편 수</p>
+        <table className="info-board">
+          <colgroup>
+            <col width="8%" />
+            <col width="7%" />
+            <col width="7%" />
+            <col width="7%" />
+            <col width="7%" />
+            <col width="7%" />
+            <col width="7%" />
+            <col width="7%" />
+            <col width="7%" />
+            <col width="7%" />
+            <col width="7%" />
+            <col width="7%" />
+            <col width="7%" />
+            <col width="7%" />
+          </colgroup>
+          <tr>
+            <th>Fleet</th>
+            <th>01</th>
+            <th>02</th>
+            <th>03</th>
+            <th>04</th>
+            <th>05</th>
+            <th>06</th>
+            <th>07</th>
+            <th>08</th>
+            <th>09</th>
+            <th>10</th>
+            <th>11</th>
+            <th>12</th>
+            <th>TTL</th>
+          </tr>
+          <tr>
+            <td>220</td>
+            <td className="right">124</td>
+            <td className="right">947</td>
+            <td className="right">365</td>
+            <td className="right">1,120</td>
+            <td className="right">124</td>
+            <td className="right">947</td>
+            <td className="right"></td>
+            <td className="right"></td>
+            <td className="right"></td>
+            <td className="right"></td>
+            <td className="right"></td>
+            <td className="right"></td>
+            <td className="right">2,556</td>
+          </tr>
+          <tr>
+            <td>321</td>
+            <td className="right">96</td>
+            <td className="right">762</td>
+            <td className="right">289</td>
+            <td className="right">799</td>
+            <td className="right">96</td>
+            <td className="right">762</td>
+            <td className="right"></td>
+            <td className="right"></td>
+            <td className="right"></td>
+            <td className="right"></td>
+            <td className="right"></td>
+            <td className="right"></td>
+            <td className="right">1,946</td>
+          </tr>
+          <tr>
+            <td>330</td>
+            <td className="right">169</td>
+            <td className="right">1,165</td>
+            <td className="right">540</td>
+            <td className="right">1,558</td>
+            <td className="right">169</td>
+            <td className="right">1,165</td>
+            <td className="right"></td>
+            <td className="right"></td>
+            <td className="right"></td>
+            <td className="right"></td>
+            <td className="right"></td>
+            <td className="right"></td>
+            <td className="right">3,432</td>
+          </tr>
+          <tr>
+            <td>380</td>
+            <td className="right">10</td>
+            <td className="right">105</td>
+            <td className="right">30</td>
+            <td className="right">89</td>
+            <td className="right">10</td>
+            <td className="right">105</td>
+            <td className="right"></td>
+            <td className="right"></td>
+            <td className="right"></td>
+            <td className="right"></td>
+            <td className="right"></td>
+            <td className="right"></td>
+            <td className="right">234</td>
+          </tr>
+          <tr>
+            <td>737</td>
+            <td className="right">229</td>
+            <td className="right">1,597</td>
+            <td className="right">738</td>
+            <td className="right">2,102</td>
+            <td className="right">229</td>
+            <td className="right">1,597</td>
+            <td className="right"></td>
+            <td className="right"></td>
+            <td className="right"></td>
+            <td className="right"></td>
+            <td className="right"></td>
+            <td className="right"></td>
+            <td className="right">4,666</td>
+          </tr>
+          <tr>
+            <td>747</td>
+            <td className="right">71</td>
+            <td className="right">629</td>
+            <td className="right">254</td>
+            <td className="right">743</td>
+            <td className="right">71</td>
+            <td className="right">629</td>
+            <td className="right"></td>
+            <td className="right"></td>
+            <td className="right"></td>
+            <td className="right"></td>
+            <td className="right"></td>
+            <td className="right"></td>
+            <td className="right">1,697</td>
+          </tr>
+          <tr>
+            <td>777</td>
+            <td className="right">219</td>
+            <td className="right">1,528</td>
+            <td className="right">656</td>
+            <td className="right">1,940</td>
+            <td className="right">219</td>
+            <td className="right">1,528</td>
+            <td className="right"></td>
+            <td className="right"></td>
+            <td className="right"></td>
+            <td className="right"></td>
+            <td className="right"></td>
+            <td className="right"></td>
+            <td className="right">4,343</td>
+          </tr>
+          <tr>
+            <td>787</td>
+            <td className="right">52</td>
+            <td className="right">377</td>
+            <td className="right">167</td>
+            <td className="right">490</td>
+            <td className="right">52</td>
+            <td className="right">377</td>
+            <td className="right"></td>
+            <td className="right"></td>
+            <td className="right"></td>
+            <td className="right"></td>
+            <td className="right"></td>
+            <td className="right"></td>
+            <td className="right">1,086</td>
+          </tr>
+          <tr className="cons-bottom">
+            <th>Total</th>
+            <td className="right">970</td>
+            <td className="right">7,110</td>
+            <td className="right">3,309</td>
+            <td className="right">8,841</td>
+            <td className="right">970</td>
+            <td className="right">7,110</td>
+            <td className="right"></td>
+            <td className="right"></td>
+            <td className="right"></td>
+            <td className="right"></td>
+            <td className="right"></td>
+            <td className="right"></td>
+            <td className="right">19,960</td>
+          </tr>
+        </table>
+      </div>
+      {/*//테이블영역 */}
     </>
   );
 }
