922fc0fd36a4cedfa6543a73ee0c4a970d1845c3
jessica0943 <jessica0943@gmail.com>
2024-09-30 10:10:23 +0900

    결제정보

--- src/components/publish/modal/PayGroupModal.tsx
+++ src/components/publish/modal/PayGroupModal.tsx
@@ -28,21 +28,48 @@ function PayGroupModal(props) {
     >
       <div className="popup-container">
         <h3 className="pop_title">결제그룹정보 설정</h3>
-        <div className="pop_lg_cont_box">
+        <div className="pop_cont">
           <div className="pop_flex_group">
             <div className="editbox">
+              <div className="form-table line">
+                <div className="form-cell wid100">
+                  <div className="form-group wid100">
+                    <div className="box-view-list">
+                      <ul className="view-list">
+                        <li className="accumlate-list">
+                          <label className="t-label">
+                            결재요청 제목 <span className="required">*</span>
+                          </label>
+                          <span className="text-desc-type1">파라미터로 넘겨받음.</span>
+                        </li>
+                      </ul>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <hr className="line"></hr>
+              <div className="form-table line">
+                <div className="form-cell wid100">
+                  <div className="form-group wid100">
+                    <div className="box-view-list">
+                      <ul className="view-list">
+                        <li className="accumlate-list">
+                          <label className="t-label">
+                            결재구분 <span className="required">*</span>
+                          </label>
+                          <span className="text-desc-type1">파라미터로 넘겨받음.</span>
+                        </li>
+                      </ul>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <hr className="line"></hr>
+
               <div className="form-table">
                 <div className="form-cell wid50">
                   <div className="form-group wid100">
-                    <AppSelect label={'결재Group 명'} />
-                  </div>
-                  <div className="btn-area inbtn">
-                    <button type="button" name="button" className="btn-x-sm btn_text btn-darkblue-line">
-                      신규
-                    </button>
-                    <button type="button" name="button" className="ml3 btn-x-sm btn_text btn-darkgray-line">
-                      삭제
-                    </button>
+                    <AppTextInput label="결재요청 메시지" />
                   </div>
                 </div>
               </div>
@@ -54,10 +81,11 @@ function PayGroupModal(props) {
                   <div className="form-group wid100">
                     <div className="UserChicebox error">
                       <div className="form-group wid100">
-                        <AppAutoComplete label="d" />
+                        <AppSelect label={'결재 Group을 선택해 주세요.'} />
+                        <AppAutoComplete label="" className="mt5" />
                         <label htmlFor="file" className="file-label">
-                          결재Group Member
-                          <span className="required"></span>
+                          결재선
+                          <span className="required">*</span>
                         </label>
                       </div>
                       <div className="form-group wid100 mt10">
@@ -108,11 +136,6 @@ function PayGroupModal(props) {
                       </div>
                     </div>
                   </div>
-                  <div className="btn-area">
-                    <button type="button" name="button" className="btn-x-sm btn_text btn-darkblue-line">
-                      초기화
-                    </button>
-                  </div>
                 </div>
               </div>
 
@@ -123,16 +146,126 @@ function PayGroupModal(props) {
 
         <div className="pop_btns">
           <button className="btn_text text_color_neutral-10 btn_confirm" onClick={closeModal}>
-            저장
+            결제Group설정
           </button>
-          <button disabled className="btn_text btn-disabled btn-type01">
-            취소
+          <button className="btn_text text_color_neutral-90 btn_close" onClick={closeModal}>
+            닫기
+          </button>
+          <button className="btn_text text_color_neutral-10 btn_confirm" onClick={closeModal}>
+            확인
           </button>
         </div>
         <span className="pop_close">X</span>
 
         {/*레이어팝업 */}
         <div className="modal-overlay"></div>
+        <div className="modal">
+          <div className="pop_lg_cont_box">
+            <h3 className="pop_title">결제그룹정보 설정</h3>
+            <div className="pop_flex_group">
+              <div className="editbox">
+                <div className="form-table">
+                  <div className="form-cell wid50">
+                    <div className="form-group wid100">
+                      <AppSelect label={'결재Group 명'} />
+                    </div>
+                    <div className="btn-area inbtn">
+                      <button type="button" name="button" className="btn-x-sm btn_text btn-darkblue-line">
+                        신규
+                      </button>
+                      <button type="button" name="button" className="ml3 btn-x-sm btn_text btn-darkgray-line">
+                        삭제
+                      </button>
+                    </div>
+                  </div>
+                </div>
+
+                <hr className="line"></hr>
+
+                <div className="form-table">
+                  <div className="form-cell">
+                    <div className="form-group wid100">
+                      <div className="UserChicebox error">
+                        <div className="form-group wid100">
+                          <AppAutoComplete label="d" />
+                          <label htmlFor="file" className="file-label">
+                            결재Group Member
+                            <span className="required"></span>
+                          </label>
+                        </div>
+                        <div className="form-group wid100 mt10">
+                          <div className="SelectedList memberClass">
+                            <ul>
+                              <li>
+                                <span className="num">1</span>
+                                <div className="Info">
+                                  <div className="Name">홍길동 (170****)</div>
+                                  <div className="Dept">상무대우수석사무장 / (주)대한항공</div>
+                                </div>
+                                <div className="column-box">
+                                  <span className="column-btn">
+                                    <a href="javascript:void(0);">
+                                      <span className="up">up</span>
+                                    </a>
+                                    <a href="javascript:void(0);">
+                                      <span className="down">down</span>
+                                    </a>
+                                  </span>
+                                </div>
+                                <a href="javascript:void(0);">
+                                  <span className="delete">X</span>
+                                </a>
+                              </li>
+                              <li>
+                                <span className="num">2</span>
+                                <div className="Info">
+                                  <div className="Name">홍길동 (170****)</div>
+                                  <div className="Dept">상무대우수석사무장 / (주)대한항공</div>
+                                </div>
+                                <div className="column-box">
+                                  <span className="column-btn">
+                                    <a href="javascript:void(0);">
+                                      <span className="up">up</span>
+                                    </a>
+                                    <a href="javascript:void(0);">
+                                      <span className="down">down</span>
+                                    </a>
+                                  </span>
+                                </div>
+                                <a href="javascript:void(0);">
+                                  <span className="delete">X</span>
+                                </a>
+                              </li>
+                            </ul>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                    <div className="btn-area">
+                      <button type="button" name="button" className="btn-x-sm btn_text btn-darkblue-line">
+                        초기화
+                      </button>
+                    </div>
+                  </div>
+                </div>
+
+                <hr className="line"></hr>
+              </div>
+            </div>
+          </div>
+          <div className="pop_btns">
+            <button className="btn_text text_color_neutral-10 btn_confirm" onClick={closeModal}>
+              저장
+            </button>
+            <button disabled className="btn_text text_color_neutral-90 btn_close">
+              닫기
+            </button>
+          </div>
+        </div>
+        {/*//레이어팝업 */}
+
+        {/*레이어팝업 */}
+        {/* <div className="modal-overlay"></div>
         <div className="modal">
           <div className="pop_lg_cont_box">
             <div className="pop_flex_group">
@@ -152,11 +285,11 @@ function PayGroupModal(props) {
             <button className="btn_text text_color_neutral-10 btn_confirm" onClick={closeModal}>
               저장
             </button>
-            <button disabled className="btn_text btn-disabled btn-type01">
+            <button disabled className="btn_text text_color_neutral-90 btn_close">
               취소
             </button>
           </div>
-        </div>
+        </div> */}
         {/*//레이어팝업 */}
       </div>
 
