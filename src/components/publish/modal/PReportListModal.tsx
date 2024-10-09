30a04b1576470ffbfef206f4e23e6920935bfefa
jessica0943 <jessica0943@gmail.com>
2024-10-09 19:12:49 +0900

    반려사유

--- src/components/publish/modal/PReportListModal.tsx
+++ src/components/publish/modal/PReportListModal.tsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import Modal from 'react-modal';
+import AppTextArea from '@/components/common/AppTextArea';
 
 function ReportListModal(props) {
   const { isOpen, closeModal } = props;
@@ -17,7 +18,15 @@ function ReportListModal(props) {
       <div className="popup-container">
         <h3 className="pop_title">반려사유 입력</h3>
         <div className="pop_cont">
-          <div className="tc">최종결제 반려 사유입니다.</div>
+          <div className="tc mt10">
+            <div className="form-table line">
+              <div className="form-cell wid100">
+                <div className="form-group wid100">
+                  <AppTextArea label={'반려사유'} style={{ width: '100%', height: 100 }} />
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
         <div className="pop_btns">
           <button className="btn_text text_color_neutral-90 btn_close" onClick={closeModal}>
