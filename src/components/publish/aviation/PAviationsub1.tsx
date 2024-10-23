0546c066701c0126c4d8dcf816af065e3a71c1cf
jessica0943 <jessica0943@gmail.com>
2024-10-23 15:40:30 +0900

    항공안전서브메인수정

--- src/components/publish/aviation/PAviationsub1.tsx
+++ src/components/publish/aviation/PAviationsub1.tsx
@@ -4,9 +4,14 @@ import useAppStore from '@/store/useAppStore';
 import AppSelect from '@/components/common/AppSelect';
 import mainphoto from '@/resources/images/av-img.jpg';
 import mainphoto2 from '@/resources/images/img.jpg';
-import chartasr from '@/resources/images/icon_asr.svg';
-import chartmsr from '@/resources/images/MSR-box.svg';
-import Slider from 'react-slick';
+import asrimg from '@/resources/images/icon_asr.svg';
+import csrimg from '@/resources/images/icon_csr.svg';
+import msrimg from '@/resources/images/icon_msr.svg';
+import gsrimg from '@/resources/images/icon_gsr.svg';
+import dsrimg from '@/resources/images/icon_dsr.svg';
+import rsrimg from '@/resources/images/icon_rsr.svg';
+import foqaimg from '@/resources/images/icon_foqa.svg';
+import hrimg from '@/resources/images/icon_hr.svg';
 
 function PAviationsub1() {
   const { setIsAviationPortal } = useStore(useAppStore, (state) => state) as any;
@@ -49,7 +54,7 @@ function PAviationsub1() {
               <div className="tit-wrap">Air Safety Report</div>
               <a href="javascript:void(0);">
                 <i className="icon-img">
-                  <img src={chartasr} className="" alt="ASR" />
+                  <img src={asrimg} className="" alt="ASR" />
                 </i>
               </a>
             </div>
@@ -59,7 +64,7 @@ function PAviationsub1() {
               <div className="tit-wrap">객실안전보고서</div>
               <a href="javascript:void(0);">
                 <i className="icon-img">
-                  <img src={chartasr} className="" alt="ASR" />
+                  <img src={csrimg} className="" alt="CSR" />
                 </i>
               </a>
             </div>
@@ -69,47 +74,47 @@ function PAviationsub1() {
               <div className="tit-wrap">정비안전보고서</div>
               <a href="javascript:void(0);">
                 <i className="icon-img">
-                  <img src={chartasr} className="" alt="ASR" />
+                  <img src={msrimg} className="" alt="MSR" />
                 </i>
               </a>
             </div>
           </div>
           <div className="av-area">
             <div className="av-box">
+              <div className="tit-wrap">지상안전보고서</div>
               <a href="javascript:void(0);">
-                <div className="tit-wrap">지상안전보고서</div>
                 <i className="icon-img">
-                  <img src={chartasr} className="" alt="ASR" />
+                  <img src={gsrimg} className="" alt="GSR" />
                 </i>
               </a>
             </div>
           </div>
           <div className="av-area">
             <div className="av-box">
+              <div className="tit-wrap">운항관리사안전보고서</div>
               <a href="javascript:void(0);">
-                <div className="tit-wrap">운항관리사안전보고서</div>
                 <i className="icon-img">
-                  <img src={chartasr} className="" alt="ASR" />
+                  <img src={dsrimg} className="" alt="DSR" />
                 </i>
               </a>
             </div>
           </div>
           <div className="av-area">
             <div className="av-box">
+              <div className="tit-wrap">램프안전보고서</div>
               <a href="javascript:void(0);">
-                <div className="tit-wrap">램프안전보고서</div>
                 <i className="icon-img">
-                  <img src={chartasr} className="" alt="ASR" />
+                  <img src={rsrimg} className="" alt="RSR" />
                 </i>
               </a>
             </div>
           </div>
           <div className="av-area">
             <div className="av-box">
+              <div className="tit-wrap">FOQA</div>
               <a href="javascript:void(0);">
-                <div className="tit-wrap">FOQA</div>
                 <i className="icon-img">
-                  <img src={chartasr} className="" alt="ASR" />
+                  <img src={foqaimg} className="" alt="FOQA" />
                 </i>
               </a>
             </div>
@@ -119,7 +124,7 @@ function PAviationsub1() {
               <div className="tit-wrap">Hazard Report</div>
               <a href="javascript:void(0);">
                 <i className="icon-img">
-                  <img src={chartasr} className="" alt="ASR" />
+                  <img src={hrimg} className="" alt="HRD" />
                 </i>
               </a>
             </div>
