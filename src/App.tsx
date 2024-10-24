9675c5d1947d952be7d23004ce990e1eb13036b2
안용성 <pj.yamdeng2@kalmate.net>
2024-10-24 16:49:56 +0900

    resize 이벤트 핸들러

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,12 @@ import useOccupationRoute from './components/publish/layout/useOccupationRoute';
 import useAdminRoute from './components/publish/layout/useAdminRoute';
 import useModalRoute from './components/publish/layout/useModalRoute';
 import useMobileRoute from './components/publish/layout/useMobileRoute';
+import { useEffect } from 'react';
+
+const handleResizeEvent = () => {
+  const vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+};
 
 function App() {
   const commonRoute = useCommonRoute();
@@ -15,6 +21,15 @@ function App() {
   const modalRoute = useModalRoute();
   const adminRoute = useAdminRoute();
   const mobileRoute = useMobileRoute();
+
+  useEffect(() => {
+    handleResizeEvent();
+
+    window.addEventListener('resize', handleResizeEvent);
+    return () => {
+      window.removeEventListener('resize', handleResizeEvent);
+    };
+  }, []);
 
   return (
     <div>
